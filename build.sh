8fcd09f5ff19e12e931df4599052df590811d297
vanillovv <pizdecvanilla@gmail.com>
2026-01-30 18:13:53 +0300

    any swaging

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-INCLUDE_DIR="/usr/lib/jvm/java-25-temurin/include"
-x86_64-w64-mingw32-g++ -shared -I"$INCLUDE_DIR" -I"$INCLUDE_DIR/linux" -o library.dll library.cpp -static-libgcc -static-libstdc++ -Wl,--add-stdcall-alias
