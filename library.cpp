f75b7d2c8571d68a8fcc81bd1dd67b4b88975718
vanillovv <pizdecvanilla@gmail.com>
2026-01-30 20:45:33 +0300

    any changes

--- library.cpp
+++ library.cpp
@@ -1,20 +1,20 @@
 #include "windows.h"
 #include "library.h"
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
 
 static std::map<int, HANDLE> pipes;
 static int g_index = 0;
 
-JNIEXPORT jint JNICALL Java_ru_vanilla_ipc_Native_create(JNIEnv* env, jobject, jstring name) {
+JNIEXPORT jint JNICALL Java_ru_vanilla_ipc_Native_create(JNIEnv* env, jclass, jstring name) {
     if (!name) return -1;
 
     const char *cstr = env->GetStringUTFChars(name, NULL);
 
     HANDLE pipe = CreateFile(
         cstr,
-        GENERIC_READ,
+        GENERIC_READ | GENERIC_WRITE,
         0,
         NULL,
         OPEN_EXISTING,
@@ -33,7 +33,7 @@ JNIEXPORT jint JNICALL Java_ru_vanilla_ipc_Native_create(JNIEnv* env, jobject, j
     return index;
 }
 
-JNIEXPORT void JNICALL Java_ru_vanilla_ipc_Native_write(JNIEnv* env, jobject, jint index, jbyteArray bytes) {
+JNIEXPORT void JNICALL Java_ru_vanilla_ipc_Native_write(JNIEnv* env, jclass, jint index, jbyteArray bytes) {
     auto it = pipes.find(index);
     if (it == pipes.end()) return;
 
@@ -45,12 +45,10 @@ JNIEXPORT void JNICALL Java_ru_vanilla_ipc_Native_write(JNIEnv* env, jobject, ji
     DWORD dword;
     BOOL write = WriteFile(pipe, data, length, &dword, NULL);
 
-    if (!write) return
-
     env->ReleaseByteArrayElements(bytes, data, 0);
 }
 
-JNIEXPORT jbyteArray JNICALL Java_ru_vanilla_ipc_Native_read(JNIEnv* env, jobject, jint index, jint size, jint offset) {
+JNIEXPORT jbyteArray JNICALL Java_ru_vanilla_ipc_Native_read(JNIEnv* env, jclass, jint index, jint size, jint offset) {
     auto it = pipes.find(index);
 
     if (it == pipes.end() || size <= 0) {
@@ -78,7 +76,7 @@ JNIEXPORT jbyteArray JNICALL Java_ru_vanilla_ipc_Native_read(JNIEnv* env, jobjec
     return byteArray;
 }
 
-JNIEXPORT void JNICALL Java_ru_vanilla_ipc_Native_close(JNIEnv*, jobject, jint index) {
+JNIEXPORT void JNICALL Java_ru_vanilla_ipc_Native_close(JNIEnv*, jclass, jint index) {
     auto it = pipes.find(index);
     if (it == pipes.end()) return;
 
--- library.h
+++ library.h
@@ -3,9 +3,9 @@
 
 #include <jni.h>
 
-JNIEXPORT jint       JNICALL Java_ru_vanilla_ipc_Native_create(JNIEnv*, jobject, jstring);
-JNIEXPORT void       JNICALL Java_ru_vanilla_ipc_Native_write(JNIEnv*, jobject, jint, jbyteArray);
-JNIEXPORT jbyteArray JNICALL Java_ru_vanilla_ipc_Native_read(JNIEnv*, jobject, jint, jint, jint);
-JNIEXPORT void       JNICALL Java_ru_vanilla_ipc_Native_close(JNIEnv*, jobject, jint);
+JNIEXPORT jint       JNICALL Java_ru_vanilla_ipc_Native_create(JNIEnv*, jclass, jstring name);
+JNIEXPORT void       JNICALL Java_ru_vanilla_ipc_Native_write(JNIEnv*, jclass, jint index, jbyteArray);
+JNIEXPORT jbyteArray JNICALL Java_ru_vanilla_ipc_Native_read(JNIEnv*, jclass, jint index, jint size, jint offset);
+JNIEXPORT void       JNICALL Java_ru_vanilla_ipc_Native_close(JNIEnv*, jclass, jint index);
 
 #endif // IPC_LIBRARY_LIBRARY_H
